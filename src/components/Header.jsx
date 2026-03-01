35214fae80a43dfe21d8e87e13b7c9ee4b0820f9
MoebiusSt <deine-email@beispiel.de>
2026-03-01 17:34:18 +0100

    Replace lower quality track 02 with higher quality track 21 in hiphop directory

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ export default function Header() {
     <header className="header">
       <h1 className="header__title">Metamorphosen</h1>
       <p className="header__subtitle">
-        Klangwelten zwischen Zerfall und Neuerfindung – ein musikalisches Projekt
-        über die stetige Wandlung von Form, Geist und Frequenz.
+      I set out to write a song for my son. The idea was to ironically rewrite the song “Feliz Navidad” to “Methylphenidate,”. This, together with the launch of “Sonauto v3”, an epoch-making free-to-use AI specialized on music inferencegave me reason to play. This turned into a five-day exploration,scratching the surface of its capabilities. How, HOW(!) do they do this?!
       </p>
     </header>
   );
