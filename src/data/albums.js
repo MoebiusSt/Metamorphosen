ca548351ec84882f901debbf9046a8f8fc650a7f
Stephan Möbius <120787945+MoebiusSt@users.noreply.github.com>
2026-03-02 20:31:16 +0100

    Change title of track 8 to NOT reference Artists at all.

--- src/data/albums.js
+++ src/data/albums.js
@@ -75,7 +75,7 @@ const albums = [
       { number: 5, title: "Hey, Du Kleine", file: "music/hamburger-schule/Metamorphosen__Im_Kreissaal_der_Gedanken_05.mp3" },
       { number: 6, title: "Geschluckt es", file: "music/hamburger-schule/Metamorphosen__Im_Kreissaal_der_Gedanken_06.mp3" },
       { number: 7, title: "Der Schritt zur Tat", file: "music/hamburger-schule/Metamorphosen__Im_Kreissaal_der_Gedanken_07.mp3" },
-      { number: 8, title: "Myrna Loy", file: "music/hamburger-schule/Metamorphosen__Im_Kreissaal_der_Gedanken_08.mp3" },
+      { number: 8, title: "Samt im Getriebe", file: "music/hamburger-schule/Metamorphosen__Im_Kreissaal_der_Gedanken_08.mp3" },
       { number: 9, title: "Charlotte und Farben", file: "music/hamburger-schule/Metamorphosen__Im_Kreissaal_der_Gedanken_09.mp3" },
       { number: 4, title: "Endlich funktioniert's!", file: "music/hamburger-schule/Metamorphosen__Im_Kreissaal_der_Gedanken_04.mp3" },
       { number: 10, title: "Auf dem Boden der Tatsachen", file: "music/hamburger-schule/Metamorphosen__Im_Kreissaal_der_Gedanken_10.mp3" },
